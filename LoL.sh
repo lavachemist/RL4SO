f7e87e34312acadd78493dc884533f284a93aaa0
lavachemist <48248311+lavachemist@users.noreply.github.com>
2022-09-13 19:37:50 -0400

    Add files via upload

---
+++ LoL.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+WINEPREFIX=${BASH_SOURCE%/*}/LoL ${BASH_SOURCE%/*}/wine/lutris-ge-lol-7.14-1-x86_64/bin/wine ${BASH_SOURCE%/*}/LoL/drive_c/Riot\ Games/Riot\ Client/RiotClientServices.exe --launch-product=league_of_legends --launch-patchline=live
